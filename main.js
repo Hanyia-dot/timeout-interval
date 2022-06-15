20727bd3a808c8a6a7c4529b6e2d22124f9bdbde
Hanna Dobrowolska <tak.obiektyw.nie@gmail.com>
2022-06-15 23:28:12 +0200

    add basic code to main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+const spnText = document.querySelector('.text');
+const spnCursor = document.querySelector('.cursor');
+const txt = ['tekst1', 'tekst2', 'tekst3']
+
+
+const addLetter = () => {
+
+}
+
+
+addLetter();
+
+
+
+const cursorAnimation = () => {
+    spnCursor.classList.toggle('active');
+}
+setInterval(cursorAnimation, 400);
